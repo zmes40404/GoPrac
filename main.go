dc012e7d244f8e0363502412cb45dd357cf2de43
Maton Wang <zmes40404@gmail.com>
2025-08-18 14:39:00 +0800

    Add: the note of "11.1.4 LevelDB for loop"
    1. Add: the note of "11.1.4 LevelDB for loop", corresponds to 56 episode on Youtube.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 
 func main() {
-	note.LeveldbBasic()
+	note.LeveldbIterate()
 
 	// note.MainHeartbeatMoniter() // function to demonstrate heartbeat monitoring using ticker in "TimerTickerExtension.go"
 	// note.TimerTimeoutControl() // function to demonstrate timer timeout control in "TimerTickerExtension.go"
--- note/db.go
+++ note/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/syndtr/goleveldb/leveldb"
+	leveldbUtil "github.com/syndtr/goleveldb/leveldb/util"
 )
 
 // 11.1 Basic Usage of LevelDB
@@ -19,6 +20,10 @@ func LeveldbBasic() {
 	data, _ := db.Get([]byte("user-1"), nil)
 	fmt.Println("data=", string(data))
 
+	// 判斷資料是否存在
+	ok, _ := db.Has([]byte("user-1"), nil)
+	fmt.Println("Has \"user-1\" ?", ok)
+
 	//批量操作
 	batch := new(leveldb.Batch)
 	batch.Put([]byte("user-2"), []byte("{\"username\":\"2\"}"))
@@ -32,4 +37,56 @@ func LeveldbBasic() {
 	data, _ = db.Get([]byte("user-3"), nil)
 	fmt.Println("data=", string(data))
 }
- 
+
+// 11.1.4 LevelDB for loop
+func LeveldbIterate() {
+	db, err := leveldb.OpenFile("leveldb", nil)
+	if err != nil{
+		panic(err)
+	}
+	defer db.Close()
+	batch := new(leveldb.Batch)
+	for i:=1; i<11; i++ {
+		batch.Put(
+			[]byte(fmt.Sprintf("user-%v", i)),
+			[]byte(fmt.Sprintf("{\"name\":\"u%v\"}", i)))
+	}
+	db.Write(batch, nil)
+
+	// 遍歷指定範圍的資料、&leveldbUtil.Range處填nil為完整資料庫
+	iter := db.NewIterator(
+		&leveldbUtil.Range{Start: []byte("user-3"), Limit: []byte("user-8")}, nil) // 從user-3開始遍歷到user-7
+	
+	for iter.Next(){
+		fmt.Printf("%v=%v\n", string(iter.Key()), string(iter.Value()))
+	}
+	iter.Release()
+	err = iter.Error()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println()
+	//遍歷指定前綴(開頭)的資料
+	iter = db.NewIterator(leveldbUtil.BytesPrefix([]byte("user-")), nil)	// 篩選所有 key 以 "user-" 為開頭的資料項目。
+	/*這邊的iterator是一個interface型別，這個 Iterator 是來自 github.com/syndtr/goleveldb/leveldb/iterator.Iterator，針對 LevelDB 優化設計，支援範圍查詢、前綴匹配、延遲加載等功能。
+	type Iterator interface {
+	Next() bool
+    Prev() bool
+    Seek(key []byte) bool
+    First() bool
+    Last() bool
+    Key() []byte
+    Value() []byte
+    Error() error
+    Release()
+	}
+	*/
+	for iter.Next() {
+		fmt.Printf("%v=%v\n", string(iter.Key()), string(iter.Value()))
+	}
+	iter.Release()
+	err = iter.Error()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
